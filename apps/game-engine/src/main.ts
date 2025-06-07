f3112e6147d9af83160aa33d4a9461d2db0e664c
Charlie <fishgills@fishgills.net>
2025-06-07 00:13:19 +0000

    chore: remove game engine application files and configurations

--- apps/game-engine/src/main.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const host = process.env.HOST ?? '0.0.0.0';
-const port = process.env.PORT ? Number(process.env.PORT) : 3002;
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
