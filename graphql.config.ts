e6d4d011226d2f45d7b3572f0c15bbab95bcb54f
Charlie Davis <fishgills@fishgills.net>
2025-08-24 22:44:17 -0700

    refactor: Simplify MovePlayer mutation response and add GraphQL configuration file

---
+++ graphql.config.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// .graphqlrc.ts or graphql.config.ts
+export default {
+  projects: {
+    bot: {
+      schema: ['world-schema.gql', 'dm-schema.gql'],
+      documents: ['apps/bot/src/graphql/*.graphql'],
+      extensions: {
+        endpoints: {
+          default: {
+            url: 'https://localhost:3000/graphql/',
+          },
+        },
+      },
+    },
+    dm: {
+      schema: ['world-schema.gql'],
+      documents: ['apps/dm/**/*.graphql'],
+      extensions: {
+        endpoints: {
+          default: {
+            url: 'https://localhost:3000/graphql/',
+          },
+        },
+      },
+    },
+  },
+};
