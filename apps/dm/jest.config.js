dad2c74946e3918fac8867bfd84440af4a67b73b
Charlie Davis <fishgills@fishgills.net>
2025-10-04 20:51:14 -0700

    refactor: update Jest configuration for dm and engine to enhance module handling

--- apps/dm/jest.config.js
+++ apps/dm/jest.config.js
@@ -2,13 +2,5 @@ import baseConfig from '../../jest.base.config.cjs';
 
 export default {
   ...baseConfig,
-  displayName: 'dm',
-  moduleNameMapper: {
-    '^@mud/constants$': '<rootDir>/../../libs/constants/src/constants.ts',
-    '^@mud/database$': '<rootDir>/../../libs/database/src/index.ts',
-    '^@mud/gcp-auth$': '<rootDir>/../../libs/gcp-auth/src/gcp-auth.ts',
-    '^@mud/(.*)$': '<rootDir>/../../libs/$1/src',
-  },
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  displayName: '@mud/dm',
 };
--- libs/engine/jest.config.js
+++ libs/engine/jest.config.js
@@ -3,5 +3,5 @@ import baseConfig from '../../jest.base.config.cjs';
 export default {
   ...baseConfig,
   displayName: '@mud/engine',
-  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  passWithNoTests: true,
 };
